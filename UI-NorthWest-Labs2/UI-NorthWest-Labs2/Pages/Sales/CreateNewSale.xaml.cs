930e20ca93fa6709a9ba184a14df7d5a075941d1
Landon <lando13@gmail.com>
2014-12-02 19:15:32 -0700

    NEW UI!

---
+++ UI-NorthWest-Labs2/UI-NorthWest-Labs2/Pages/Sales/CreateNewSale.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI_NorthWest_Labs2.Pages.Sales
+{
+    /// <summary>
+    /// Interaction logic for CreateNewSale.xaml
+    /// </summary>
+    public partial class CreateNewSale : UserControl
+    {
+        public CreateNewSale()
+        {
+            InitializeComponent();
+        }
+    }
+}
