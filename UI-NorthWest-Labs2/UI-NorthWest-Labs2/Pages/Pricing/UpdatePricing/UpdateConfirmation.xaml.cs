3904b93d1406c04606d614e08aca86954b108134
taylorcurtis <taylorcurtis@gmail.com>
2014-12-03 15:25:55 -0700

    Update Pricing Use Case

---
+++ UI-NorthWest-Labs2/UI-NorthWest-Labs2/Pages/Pricing/UpdatePricing/UpdateConfirmation.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace UI_NorthWest_Labs2.Pages.Pricing.UpdatePricing
+{
+    /// <summary>
+    /// Interaction logic for BasicPage1.xaml
+    /// </summary>
+    public partial class UpdateConfirmation : ModernDialog
+    {
+        public UpdateConfirmation()
+        {
+            InitializeComponent();
+            this.Buttons = new Button[] { this.OkButton };
+
+
+        }
+    }
+}
---
+++ UI-NorthWest-Labs2/UI-NorthWest-Labs2/Pages/Pricing/UpdatePricing/UpdatePricing.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI_NorthWest_Labs2.Pages.Pricing.UpdatePricing
+{
+    /// <summary>
+    /// Interaction logic for BasicPage1.xaml
+    /// </summary>
+    public partial class UpdatePricing : UserControl
+    {
+        public UpdatePricing()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationCommands.BrowseBack.Execute(null, MainWindow.frame);
+            UpdateConfirmation uc = new UpdateConfirmation();
+            uc.ShowDialog();
+        }
+    }
+}
