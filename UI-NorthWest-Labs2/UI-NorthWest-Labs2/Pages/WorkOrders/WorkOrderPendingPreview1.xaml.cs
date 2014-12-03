eaf9db32ca7bede9aed1cb211d75842068f21ef2
Landon <lando13@gmail.com>
2014-12-03 15:40:30 -0700

    Created new Work Order Recieving Views

---
+++ UI-NorthWest-Labs2/UI-NorthWest-Labs2/Pages/WorkOrders/WorkOrderPendingPreview1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI_NorthWest_Labs2.Pages.WorkOrders
+{
+    /// <summary>
+    /// Interaction logic for WorkOrderPreview.xaml
+    /// </summary>
+    public partial class WorkOrderPendingPreview1 : UserControl
+    {
+        public WorkOrderPendingPreview1()
+        {
+            InitializeComponent();
+        }
+
+        private void View_Work_Order_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationCommands.GoToPage.Execute("/Pages/WorkOrders/WorkOrderInfo.xaml", MainWindow.frame);
+        }
+
+        private void Begin_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationCommands.GoToPage.Execute("/Pages/WorkOrders/ReceivedWorkOrder.xaml", MainWindow.frame);
+        }
+
+  
+    }
+}
