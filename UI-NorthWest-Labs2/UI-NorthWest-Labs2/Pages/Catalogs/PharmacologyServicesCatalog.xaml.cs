9475fcb0bb5f0873b45bacec2828ba86bd03e322
johnmeservy <meservy@gmail.com>
2014-12-03 17:18:03 -0700

    Download

--- UI-NorthWest-Labs2/UI-NorthWest-Labs2/Pages/Catalogs/PharmacologyServicesCatalog.xaml.cs
+++ UI-NorthWest-Labs2/UI-NorthWest-Labs2/Pages/Catalogs/PharmacologyServicesCatalog.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace UI_NorthWest_Labs2.Pages.Catalogs
 {
@@ -27,12 +30,29 @@ namespace UI_NorthWest_Labs2.Pages.Catalogs
 
         private void UploadCatalog_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("I hate when you guys make fun of me");
+            
+            //MessageBox.Show("I hate when you guys make fun of me");
         }
 
         private void DownloadCatalog_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("I hate when you guys make fun of me");
+            
+            SaveFileDialog saveFileDialog1 = new Microsoft.Win32.SaveFileDialog();
+
+            saveFileDialog1.Filter = "PDF files (*.PDF)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            saveFileDialog1.ShowDialog();
+
+            //if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    if ((myStream = saveFileDialog1.OpenFile()) != null)
+            //    {
+            //        // Code to write the stream goes here.
+            //        myStream.Close();
+            //    }
+            //}
+            //MessageBox.Show("I hate when you guys make fun of me");
         }
     }
 }
