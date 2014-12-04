54fbca78c52751465b2d846946fa83d1a76a5299
taylorcurtis <taylorcurtis@gmail.com>
2014-12-03 19:04:32 -0700

    Open PDF :)

--- UI-NorthWest-Labs2/UI-NorthWest-Labs2/Pages/Sales/Reports/CustomerReport.xaml.cs
+++ UI-NorthWest-Labs2/UI-NorthWest-Labs2/Pages/Sales/Reports/CustomerReport.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace UI_NorthWest_Labs2.Pages.Sales.Reports
 {
     /// <summary>
@@ -23,6 +24,24 @@ namespace UI_NorthWest_Labs2.Pages.Sales.Reports
         public CustomerReport()
         {
             InitializeComponent();
+        }
+
+        private void CreateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //set pdf name here
+            string pdfname = "campusmap";
+            
+            //add PDF folder and .pdf to filename
+            string fullname = "\\PDF\\" + pdfname + ".pdf";
+
+            //find the full filepath
+            string filepath = AppDomain.CurrentDomain.BaseDirectory;
+
+            //remove 'bin' and 'debug' folder
+            filepath = filepath.Remove(filepath.Length-10);
+
+            //open the pdf :)
+            System.Diagnostics.Process.Start(filepath + fullname);
         }
     }
 }
